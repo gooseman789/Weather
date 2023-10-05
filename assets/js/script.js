c1a38de622a978e2ac699b2ca8c06271a2dd4151
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2023-10-04 21:35:59 -0400

    finishing functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,21 +22,41 @@ searchBTN.on('click', function() {
         var cityLAT = cityAR[0]
         var cityLON = cityAR[1]
 
-
-
-        fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + cityLAT + '&lon=' + cityLON + '&appid=b364c079aaefaadfbc5f1d3a45eebea4&units=imperial', {
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + cityLAT + '&lon=' + cityLON + '&appid=b364c079aaefaadfbc5f1d3a45eebea4&units=imperial', {
 
         })
         .then(function (response1) {
             return response1.json();
         })
-        .then(function (data1){
-            var weatherLIST = data1.list 
+        .then(function(data1) {
+            var current = Object.keys(data1).map((key) => [key, data1[key]])
+            console.log(current)
+            var currentsky = current[1][1][0].description
+            var currentTemp = current[3][1].temp
+            var currentHUM = current[3][1].humidity
+            var currentWIND = current[5][1].speed
+            $('#citydate').text(dayjs().format('MM/DD/YYYY'))
+            $('#currentsky').text(currentsky)
+            $('#temp').text("Temp: " + currentTemp + "F")
+            $('#wind').text("Wind speed: " + currentWIND + "MPH")
+            $('#humidity').text("Humidity: " + currentHUM + "%")
+        })
+
+
+
+        fetch('http://api.openweathermap.org/data/2.5/forecast?lat=' + cityLAT + '&lon=' + cityLON + '&appid=b364c079aaefaadfbc5f1d3a45eebea4&units=imperial', {
+
+        })
+        .then(function (response2) {
+            return response2.json();
+        })
+        .then(function (data2){
+            var weatherLIST = data2.list 
             var weatherAR = []
             for (let i = 0; i < weatherLIST.length; i=i+8) {
                 weatherAR.push(weatherLIST[i])
             }
-
+            console.log(weatherAR)
             for (let i =0; i < weatherAR.length; i++) {
                 var citysky = weatherAR[i].weather[0].description
                 var citytemp = weatherAR[i].main.temp
@@ -44,7 +64,7 @@ searchBTN.on('click', function() {
                 var citywind = weatherAR[i].wind.speed
                 var elementID = '5day' + (i+1)
                 $('#' + elementID).addClass('dark')
-                $('#' + elementID).children('.temp').text("Temp: " + citytemp)
+                $('#' + elementID).children('.temp').text("Temp: " + citytemp + "F")
                 $('#' + elementID).children('.wind').text("Wind speed: " + citywind + "MPH")
                 $('#' + elementID).children('.humidity').text("Humidity: " + cityHUM + "%")
             }   
